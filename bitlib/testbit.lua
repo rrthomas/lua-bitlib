9a4d240ce756606f0f3c92c13fb678cbddd8dad6
Reuben Thomas <rrt@luaforge.net>
2007-11-19 22:00:30 +0000

    Make spacing consistent.

--- bitlib/testbit.lua
+++ bitlib/testbit.lua
@@ -8,43 +8,43 @@ while n > 0 do
   maxbits = maxbits + 1
 end
 
-assert (bit.band (0,0) == 0)
-assert (bit.band (0,-1) == 0)
-assert (bit.band (-1,-1) == -1)
+assert (bit.band (0, 0) == 0)
+assert (bit.band (0, -1) == 0)
+assert (bit.band (-1, -1) == -1)
 
-assert (bit.bor  (0,0) == 0)
-assert (bit.bor  (0,-1) == -1)
-assert (bit.bor  (-1,-1) == -1)
+assert (bit.bor  (0, 0) == 0)
+assert (bit.bor  (0, -1) == -1)
+assert (bit.bor  (-1, -1) == -1)
 
-assert (bit.bxor (0,0) == 0)
-assert (bit.bxor (0,-1) == -1)
-assert (bit.bxor (-1,-1) == 0)
+assert (bit.bxor (0, 0) == 0)
+assert (bit.bxor (0, -1) == -1)
+assert (bit.bxor (-1, -1) == 0)
 
 assert (bit.bnot (0) == -1)
 assert (bit.bnot (-1) == 0)
 
-assert (bit.lshift (0,0) == 0)
-assert (bit.lshift (-1,0) == -1)
+assert (bit.lshift (0, 0) == 0)
+assert (bit.lshift (-1, 0) == -1)
 
-assert (bit.rshift (0,0) == 0)
-assert (bit.rshift (-1,0) == -1)
+assert (bit.rshift (0, 0) == 0)
+assert (bit.rshift (-1, 0) == -1)
 
 for nb = 1, maxbits do
   local a = 2 ^ nb - 1
-  assert (bit.band (a,0)  == 0)
-  assert (bit.band (a,1)  == 1)
-  assert (bit.band (a,-1) == bit.cast (a))
-  assert (bit.band (a,a)  == bit.cast (a))
+  assert (bit.band (a, 0)  == 0)
+  assert (bit.band (a, 1)  == 1)
+  assert (bit.band (a, -1) == bit.cast (a))
+  assert (bit.band (a, a)  == bit.cast (a))
 
-  assert (bit.bor (a,0)  == bit.cast (a))
-  assert (bit.bor (a,1)  == bit.cast (a))
-  assert (bit.bor (a,-1) == -1)
-  assert (bit.bor (a,a)  == bit.cast (a))
+  assert (bit.bor (a, 0)  == bit.cast (a))
+  assert (bit.bor (a, 1)  == bit.cast (a))
+  assert (bit.bor (a, -1) == -1)
+  assert (bit.bor (a, a)  == bit.cast (a))
 
-  assert (bit.bxor (a,0)  == bit.cast (a))
-  assert (bit.bxor (a,1)  == bit.cast (a - 1))
-  assert (bit.bxor (a,-1) == bit.cast (-a - 1))
-  assert (bit.bxor (a,a)  == 0)
+  assert (bit.bxor (a, 0)  == bit.cast (a))
+  assert (bit.bxor (a, 1)  == bit.cast (a - 1))
+  assert (bit.bxor (a, -1) == bit.cast (-a - 1))
+  assert (bit.bxor (a, a)  == 0)
 
   assert (bit.bnot (a) == bit.cast (-1 - a))
 
