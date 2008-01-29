b992991daf747773a996757f6e26ff9f2321d240
Reuben Thomas <rrt@luaforge.net>
2008-01-29 15:24:55 +0000

    Fix punctuation.

--- bitlib/test.lua
+++ bitlib/test.lua
@@ -12,44 +12,46 @@ package.cpath = (os.getenv ("LD_LIBRARY_PATH") or ".") .. "/?.so"
 require "bit"
 
 
-assert (bit.band (0, 0) == 0)
-assert (bit.band (0, -1) == 0)
-assert (bit.band (-1, -1) == -1)
+print ("bit.bits = " .. bit.bits)
 
-assert (bit.bor  (0, 0) == 0)
-assert (bit.bor  (0, -1) == -1)
-assert (bit.bor  (-1, -1) == -1)
+assert (bit.band (0, 0) == bit.cast (0))
+assert (bit.band (0, -1) == bit.cast (0))
+assert (bit.band (-1, -1) == bit.cast (-1))
 
-assert (bit.bxor (0, 0) == 0)
-assert (bit.bxor (0, -1) == -1)
-assert (bit.bxor (-1, -1) == 0)
+assert (bit.bor  (0, 0) == bit.cast (0))
+assert (bit.bor  (0, -1) == bit.cast (-1))
+assert (bit.bor  (-1, -1) == bit.cast (-1))
 
-assert (bit.bnot (0) == -1)
-assert (bit.bnot (-1) == 0)
+assert (bit.bxor (0, 0) == bit.cast (0))
+assert (bit.bxor (0, -1) == bit.cast (-1))
+assert (bit.bxor (-1, -1) == bit.cast (0))
 
-assert (bit.lshift (0, 0) == 0)
-assert (bit.lshift (-1, 0) == -1)
+assert (bit.bnot (0) == bit.cast (-1))
+assert (bit.bnot (-1) == bit.cast (0))
 
-assert (bit.rshift (0, 0) == 0)
-assert (bit.rshift (-1, 0) == -1)
+assert (bit.lshift (0, 0) == bit.cast (0))
+assert (bit.lshift (-1, 0) == bit.cast (-1))
+
+assert (bit.rshift (0, 0) == bit.cast (0))
+assert (bit.rshift (-1, 0) == bit.cast (-1))
 
 for nb = 1, bit.bits do
   local a = 2 ^ nb - 1
   print ("nb = " .. nb .. ", a = " .. a)
-  assert (bit.band (a, 0)  == 0)
-  assert (bit.band (a, 1)  == 1)
+  assert (bit.band (a, 0)  == bit.cast (0))
+  assert (bit.band (a, 1)  == bit.cast (1))
   assert (bit.band (a, -1) == bit.cast (a))
   assert (bit.band (a, a)  == bit.cast (a))
 
   assert (bit.bor (a, 0)  == bit.cast (a))
   assert (bit.bor (a, 1)  == bit.cast (a))
-  assert (bit.bor (a, -1) == -1)
+  assert (bit.bor (a, -1) == bit.cast (-1))
   assert (bit.bor (a, a)  == bit.cast (a))
 
   assert (bit.bxor (a, 0)  == bit.cast (a))
   assert (bit.bxor (a, 1)  == bit.cast (a - 1))
   assert (bit.bxor (a, -1) == bit.cast (-a - 1))
-  assert (bit.bxor (a, a)  == 0)
+  assert (bit.bxor (a, a)  == bit.cast (0))
 
   assert (bit.bnot (a) == bit.cast (-1 - a))
 
@@ -60,11 +62,11 @@ for nb = 1, bit.bits do
 
   assert (bit.rshift (a, 1) == math.floor (a / 2))
   if nb < bit.bits then
-    assert (bit.rshift (a, nb) == 0)
+    assert (bit.rshift (a, nb) == bit.cast (0))
   end
-  assert (bit.rshift (a, nb - 1) == 1)
+  assert (bit.rshift (a, nb - 1) == bit.cast (1))
 
-  assert (bit.arshift (-1, 1) == -1)
+  assert (bit.arshift (-1, 1) == bit.cast (-1))
 end
 
 print "All bitlib tests passed"
